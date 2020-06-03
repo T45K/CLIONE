f9fd3a108ceaca5b5fcd54e603e1500c8b51c1f1
T45K <tasktas9@gmail.com>
2020-06-03 18:30:37 +0900

    impl

--- src/main/kotlin/io/github/t45k/clione/controller/GitController.kt
+++ src/main/kotlin/io/github/t45k/clione/controller/GitController.kt
@@ -1,13 +1,34 @@
 package io.github.t45k.clione.controller
 
+import io.github.t45k.clione.entity.FileChangeType
+import io.github.t45k.clione.entity.FileDiff
 import io.reactivex.rxjava3.core.Observable
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.diff.DiffAlgorithm
+import org.eclipse.jgit.diff.DiffEntry
+import org.eclipse.jgit.diff.DiffFormatter
+import org.eclipse.jgit.diff.Edit
+import org.eclipse.jgit.diff.EditList
+import org.eclipse.jgit.diff.RawText
+import org.eclipse.jgit.diff.RawTextComparator
+import org.eclipse.jgit.lib.AbbreviatedObjectId
+import org.eclipse.jgit.lib.Constants
+import org.eclipse.jgit.lib.ObjectId
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.revwalk.RevTree
+import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
+import org.eclipse.jgit.treewalk.AbstractTreeIterator
+import org.eclipse.jgit.treewalk.CanonicalTreeParser
+import org.eclipse.jgit.util.io.DisabledOutputStream
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import util.deleteRecursive
 import java.nio.file.Path
 
+/**
+ * Note: Any Variable named "fileName" in this class is string of relative path from git repository
+ */
 class GitController(private val git: Git) {
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(this::class.java)
@@ -28,8 +49,114 @@ class GitController(private val git: Git) {
     }
 
     fun deleteRepo() =
-        Observable.just(Path.of(git.repository.directory.parentFile.absolutePath))
+        Observable.just(git.repository.directory.parentFile.absoluteFile.toPath())
             .doOnSubscribe { logger.info("[START]\tdelete ${git.repository.directory.parentFile}") }
             .doOnComplete { logger.info("[END]\tdelete ${git.repository.directory.parentFile}") }
             .subscribe(::deleteRecursive)!!
+
+    fun getProjectPath(): Path = git.repository.directory.parentFile.absoluteFile.toPath()
+
+    /**
+     * Same operation as "git checkout hash"
+     */
+    fun checkout(commitHash: String) {
+        git.checkout()
+            .setName(commitHash)
+            .call()
+    }
+
+    fun findChangeFiles(oldCommitHash: String, newCommitHash: String): Pair<Set<String>, Set<String>> {
+        val oldFileNames: MutableSet<String> = mutableSetOf()
+        val newFileNames: MutableSet<String> = mutableSetOf()
+        executeDiffCommand(oldCommitHash, newCommitHash)
+            .forEach {
+                it.oldPath?.apply { oldFileNames.add(this) }
+                it.newPath?.apply { newFileNames.add(this) }
+            }
+
+        return oldFileNames to newFileNames
+    }
+
+    fun calcFileDiff(fileName: String, oldCommitHash: String, newCommitHash: String): FileDiff {
+        val diffEntries: List<DiffEntry> = executeDiffCommand(oldCommitHash, newCommitHash).filter { it.oldPath == fileName }
+        if (diffEntries.isEmpty()) {
+            // All clones in this file are STABLE
+            return FileDiff(FileChangeType.STABLE, emptyList(), fileName)
+        }
+
+        val entry: DiffEntry = diffEntries.first()
+        if (entry.changeType == DiffEntry.ChangeType.DELETE) {
+            return FileDiff(FileChangeType.DELETE, emptyList(), "")
+        }
+
+        val oldRawText: RawText = readBlob(entry.oldId)
+        val newRawText: RawText = readBlob(entry.newId)
+        val editList: EditList = DiffAlgorithm.getAlgorithm(DiffAlgorithm.SupportedAlgorithm.HISTOGRAM)
+            .diff(RawTextComparator.DEFAULT, oldRawText, newRawText)
+        val size: Int = String(oldRawText.rawContent).split("\n").size
+        return FileDiff(FileChangeType.MODIFY, mapLine(editList, size), entry.newPath)
+    }
+
+    /**
+     * Mapping line number of the file of old revision to new one
+     * Each value is increase/decrease value between old line number and new one
+     */
+    private fun mapLine(editList: EditList, size: Int): List<Int> {
+        val lineMapping: Array<Int> = Array(size + 1) { 0 }
+
+        for (edit: Edit in editList) {
+            when (edit.type) {
+                Edit.Type.INSERT -> {
+                    lineMapping[edit.beginA + 1] += edit.endB - edit.beginB
+                }
+
+                Edit.Type.DELETE -> {
+                    (edit.beginA + 1..edit.endA).forEach { lineMapping[it]-- }
+                }
+
+                Edit.Type.REPLACE -> {
+                    lineMapping[edit.beginA + 1] += edit.endB - edit.beginB
+                    (edit.beginA + 1..edit.endA).forEach { lineMapping[it]-- }
+                }
+
+                Edit.Type.EMPTY -> {
+                    // do nothing
+                }
+
+                else -> throw RuntimeException()
+            }
+        }
+
+        (1..size).forEach { lineMapping[it] += lineMapping[it - 1] }
+        return lineMapping.toList()
+    }
+
+    private fun readBlob(blobId: AbbreviatedObjectId): RawText =
+        git.repository.newObjectReader()
+            .open(blobId.toObjectId(), Constants.OBJ_BLOB)
+            .run { RawText(this.cachedBytes) }
+
+    /**
+     * Same operation as "git diff revision1..revision2"
+     */
+    private fun executeDiffCommand(oldCommitHash: String, newCommitHash: String): List<DiffEntry> {
+        val oldTreeParser: AbstractTreeIterator = prepareTreeParser(ObjectId.fromString(oldCommitHash))
+        val newTreeParser: AbstractTreeIterator = prepareTreeParser(ObjectId.fromString(newCommitHash))
+
+        return DiffFormatter(DisabledOutputStream.INSTANCE)
+            .apply { this.setRepository(git.repository) }
+            .apply { this.setDiffComparator(RawTextComparator.DEFAULT) }
+            .apply { this.isDetectRenames = true }
+            .scan(oldTreeParser, newTreeParser)
+    }
+
+    private fun prepareTreeParser(objectId: ObjectId): AbstractTreeIterator {
+        val walk = RevWalk(git.repository)
+        val commit: RevCommit = walk.parseCommit(objectId)
+        val tree: RevTree = walk.parseTree(commit.tree.id)
+        val treeParser: CanonicalTreeParser = CanonicalTreeParser()
+            .apply { this.reset(git.repository.newObjectReader(), tree) }
+        walk.dispose()
+        return treeParser
+    }
 }

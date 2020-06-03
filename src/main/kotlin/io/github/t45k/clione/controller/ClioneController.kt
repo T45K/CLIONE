21d86ee59dffbe2571cf4c0afc6dba8f12260ec5
T45K <tasktas9@gmail.com>
2020-06-03 18:43:07 +0900

    move initializer

--- src/main/kotlin/io/github/t45k/clione/controller/ClioneController.kt
+++ src/main/kotlin/io/github/t45k/clione/controller/ClioneController.kt
@@ -2,7 +2,7 @@ package io.github.t45k.clione.controller
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.github.t45k.clione.exception.NoPropertyFileExistsException
+import io.github.t45k.clione.entity.NoPropertyFileExistsException
 import io.github.t45k.clione.github.GitHubAuthenticator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -18,17 +18,15 @@ import javax.servlet.http.HttpServletRequest
 
 @RestController
 class ClioneApiController {
-    private val logger: Logger = LoggerFactory.getLogger(this::class.java)
-    private val githubWebhookSecret: String
-
-    init {
-        val bundle: ResourceBundle = ResourceBundle.getBundle("verify") ?: throw NoPropertyFileExistsException()
-        githubWebhookSecret = bundle.getString("GITHUB_WEBHOOK_SECRET")
-    }
 
     companion object {
         const val WEBHOOK_SIGNATURE: String = "x-hub-signature"
         const val WEBHOOK_EVENT: String = "x-github-event"
+
+        private val logger: Logger = LoggerFactory.getLogger(this::class.java)
+
+        private val bundle: ResourceBundle = ResourceBundle.getBundle("verify") ?: throw NoPropertyFileExistsException()
+        private val githubWebhookSecret: String = bundle.getString("GITHUB_WEBHOOK_SECRET")
     }
 
     @Autowired

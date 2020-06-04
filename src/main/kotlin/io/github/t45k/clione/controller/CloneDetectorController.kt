83499f18aa5a63d6997aa0a6bea4c99f71546e03
tsk-nkgw <tasktas9@gmail.com>
2020-06-04 13:59:48 +0900

    impl CloneDetector

--- src/main/kotlin/io/github/t45k/clione/controller/CloneDetectorController.kt
+++ src/main/kotlin/io/github/t45k/clione/controller/CloneDetectorController.kt
@@ -4,6 +4,15 @@ import io.github.t45k.clione.entity.CloneSets
 import io.github.t45k.clione.entity.IdCloneMap
 
 interface CloneDetectorController {
+
+    /**
+     * Execute clone detector on new revision.
+     * In this execution, not only clone instances but clone candidates are recorded in IdCloneMap.
+     */
     fun executeOnNewRevision(changedFiles: Set<String>): Pair<CloneSets, IdCloneMap>
+
+    /**
+     * Execute clone detector on old revision
+     */
     fun executeOnOldRevision(changedFiles: Set<String>): Pair<CloneSets, IdCloneMap>
 }

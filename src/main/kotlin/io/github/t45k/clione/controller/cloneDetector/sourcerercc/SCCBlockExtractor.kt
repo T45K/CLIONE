dfcf2953812520066f6a56f2edb61f4f18d35404
T45K <tasktas9@gmail.com>
2020-06-15 16:52:22 +0900

    add BlockExtractor

---
+++ src/main/kotlin/io/github/t45k/clione/controller/cloneDetector/sourcerercc/SCCBlockExtractor.kt
@@ -0,0 +1,19 @@
+package io.github.t45k.clione.controller.cloneDetector.sourcerercc
+
+import io.github.t45k.clione.entity.CloneStatus
+import java.nio.file.Path
+
+// TODO implementation of other languages
+interface SCCBlockExtractor {
+
+    /**
+     * Extract code blocks(= clone candidates) from target source code.
+     *
+     * @param code target source code
+     * @param filePath target path
+     * @param cloneStatus if target file is not modified between the pull request, this value will be STABLE.
+     *
+     * @return clone candidates
+     */
+    fun extract(code: String, filePath: Path, cloneStatus: CloneStatus): List<DelayCloneInstance>
+}

b135e4bef5bc47f3eaa9ca26488ec95c2546a73c
T45K <tasktas9@gmail.com>
2020-06-17 11:56:21 +0900

    雛形

--- src/main/kotlin/io/github/t45k/clione/controller/cloneDetector/sourcerercc/SourcererCCController.kt
+++ src/main/kotlin/io/github/t45k/clione/controller/cloneDetector/sourcerercc/SourcererCCController.kt
@@ -1,10 +1,14 @@
 package io.github.t45k.clione.controller.cloneDetector.sourcerercc
 
+import com.mondego.indexbased.SearchManager
 import io.github.t45k.clione.controller.cloneDetector.CloneDetectorController
 import io.github.t45k.clione.core.RunningConfig
 import io.github.t45k.clione.entity.CloneSets
 import io.github.t45k.clione.entity.CloneStatus
 import io.github.t45k.clione.entity.IdCloneMap
+import org.jgrapht.alg.clique.BronKerboschCliqueFinder
+import org.jgrapht.graph.DefaultEdge
+import org.jgrapht.graph.SimpleGraph
 import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.streams.asSequence
@@ -12,7 +16,9 @@ import kotlin.streams.asSequence
 class SourcererCCController(private val sourceCodePath: Path, private val config: RunningConfig) : CloneDetectorController {
 
     companion object {
-        const val SCC_PATH = ""
+        private const val sccPropertiesLocation = "./src/main/resources/sourcerer-cc.properties"
+        private const val candidateFileName = "candidates.query"
+        private const val resultFileName = "candidatesclones_index_WITH_FILTER.txt"
     }
 
     // TODO other languages' extension
@@ -37,12 +43,39 @@ class SourcererCCController(private val sourceCodePath: Path, private val config
             .mapIndexed { index, cloneInstance -> index to cloneInstance.setId(index) }
             .toMap()
 
+        val sccCloneInfo = generateSCCFormat()
+        Files.writeString(Path.of(sourceCodePath.toString(), "NODE", "query", candidateFileName), sccCloneInfo)
+        SearchManager.main(arrayOf("init", "8", "$sourceCodePath/", sccPropertiesLocation))
+        SearchManager.main(arrayOf("index", "8", "$sourceCodePath/", sccPropertiesLocation))
+        collectIndexFiles()
+        SearchManager.main(arrayOf("search", "8", "$sourceCodePath/", sccPropertiesLocation))
+        val results: List<String> = Files.readAllLines(Path.of(sourceCodePath.toString(), "NODE", "output8.0", resultFileName))
+
         // TODO
         return emptyList<Set<Int>>() to idCloneMap
     }
 
-    @Synchronized
-    private fun execute() {
+    private fun createCloneSets(rawResults: List<String>): CloneSets =
+        rawResults.map { it.split(',') }
+            .map { it[1].toInt() to it[3].toInt() }
+            .fold(SimpleGraph<Int, DefaultEdge>(DefaultEdge::class.java)) { graph, pair ->
+                graph.addVertex(pair.first)
+                graph.addVertex(pair.second)
+                graph.addEdge(pair.first, pair.second)
+                graph
+            }
+            .let {
+                BronKerboschCliqueFinder(it)
+                    .iterator()
+                    .asSequence()
+                    .toList()
+            }
+
+    private fun generateSCCFormat(): String {
+        return ""
+    }
+
+    private fun collectIndexFiles() {
 
     }
 }

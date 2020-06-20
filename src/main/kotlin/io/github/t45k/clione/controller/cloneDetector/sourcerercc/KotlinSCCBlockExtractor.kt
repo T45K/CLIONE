51a744f9ea3c5190ae88c4e55ef4c7c1b5c5a133
T45K <tasktas9@gmail.com>
2020-06-20 23:32:27 +0900

    impl

--- src/main/kotlin/io/github/t45k/clione/controller/cloneDetector/sourcerercc/KotlinSCCBlockExtractor.kt
+++ src/main/kotlin/io/github/t45k/clione/controller/cloneDetector/sourcerercc/KotlinSCCBlockExtractor.kt
@@ -1,20 +1,32 @@
 package io.github.t45k.clione.controller.cloneDetector.sourcerercc
 
+import io.github.t45k.clione.core.tokenizer.KotlinTokenizer
 import io.github.t45k.clione.entity.CloneStatus
-import org.jetbrains.kotlin.spec.grammar.tools.KotlinParseTree
-import org.jetbrains.kotlin.spec.grammar.tools.parseKotlinCode
-import org.jetbrains.kotlin.spec.grammar.tools.tokenizeKotlinCode
 import java.nio.file.Path
+import java.util.ArrayDeque
+import java.util.Deque
 
 class KotlinSCCBlockExtractor : SCCBlockExtractor {
 
     /**
-     * Extract clone candidates by using Kotlin/grammar-tools
+     * Extract clone candidates by Brute Force...
      */
     override fun extract(code: String, filePath: Path, cloneStatus: CloneStatus): List<Pair<LazyCloneInstance, String>> {
-        val parseTree: KotlinParseTree = parseKotlinCode(tokenizeKotlinCode(code))
+        var loc = 1
+        val leftBraceQueue: Deque<Pair<Int, Int>> = ArrayDeque() // position and line number
+        val candidates: MutableList<Pair<LazyCloneInstance, String>> = mutableListOf()
+        code.forEachIndexed { index, c ->
+            when (c) {
+                '\n' -> loc++
+                '{' -> leftBraceQueue.add(index to loc)
+                '}' -> {
+                    val (position, lineNumber) = leftBraceQueue.pop()
+                    val block: String = code.substring(position, index + 1)
+                    candidates.add(LazyCloneInstance(filePath.toString(), lineNumber, loc, cloneStatus, KotlinTokenizer().tokenize(block)) to block)
+                }
+            }
+        }
 
-        println(parseTree)
-        return emptyList()
+        return candidates
     }
 }

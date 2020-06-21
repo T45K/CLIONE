bb8156a95ded73867e284d9ff37f43d4da307c2b
T45K <tasktas9@gmail.com>
2020-06-21 20:40:05 +0900

    test exception

--- src/test/kotlin/io/github/t45k/clione/core/ConfigGeneratorKtTest.kt
+++ src/test/kotlin/io/github/t45k/clione/core/ConfigGeneratorKtTest.kt
@@ -1,7 +1,9 @@
 package io.github.t45k.clione.core
 
+import io.github.t45k.clione.entity.InvalidConfigSpecifiedException
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 internal class ConfigGeneratorKtTest {
 
@@ -19,5 +21,11 @@ internal class ConfigGeneratorKtTest {
         assertEquals(CloneDetector.SOURCERERCC, cloneDetector)
         assertEquals(Granularity.METHOD, granularity)
         assertEquals(9, similarity)
+    }
+
+    @Test
+    fun testGenerateConfigWithException() {
+        val toml = """lang = "python""""
+        assertFailsWith<InvalidConfigSpecifiedException> { generateConfig(toml) }
     }
 }

d13353ef5361a25f7a19beb6fe86d3c71e4ea37e
T45K <tasktas9@gmail.com>
2020-06-29 15:47:10 +0900

    impl antlr tokenizer

---
+++ src/main/kotlin/io/github/t45k/clione/core/tokenizer/Python3Tokenizer.kt
@@ -0,0 +1,15 @@
+package io.github.t45k.clione.core.tokenizer
+
+import antlr.python3.Python3Lexer
+import org.antlr.v4.runtime.CommonTokenStream
+
+class Python3Tokenizer : Tokenizer {
+    override fun tokenize(contents: String): List<String> =
+        contents.run { this.toCharStream() }
+            .run { Python3Lexer(this) }
+            .run { CommonTokenStream(this) }
+            .apply { this.fill() }
+            .tokens
+            .map { it.text }
+            .filter { it.isNotBlank() && it != "<EOF>" }
+}
--- src/main/kotlin/io/github/t45k/clione/core/tokenizer/Tokenizer.kt
+++ src/main/kotlin/io/github/t45k/clione/core/tokenizer/Tokenizer.kt
@@ -1,5 +1,13 @@
 package io.github.t45k.clione.core.tokenizer
 
+import org.antlr.v4.runtime.CharStream
+import org.antlr.v4.runtime.CharStreams
+
 interface Tokenizer {
     fun tokenize(contents: String): List<String>
+
+    /**
+     * Generate CharStream from String type for ANTLR
+     */
+    fun String.toCharStream(): CharStream = CharStreams.fromString(this)
 }

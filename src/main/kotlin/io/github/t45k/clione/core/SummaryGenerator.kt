005479fd9ddbe2c7a5bfb2123246d24fb60ab93a
T45K <tasktas9@gmail.com>
2020-06-30 14:10:08 +0900

    add unmergedClones

--- src/main/kotlin/io/github/t45k/clione/core/SummaryGenerator.kt
+++ src/main/kotlin/io/github/t45k/clione/core/SummaryGenerator.kt
@@ -2,23 +2,32 @@ package io.github.t45k.clione.core
 
 import io.github.t45k.clione.entity.CloneInstance
 import io.github.t45k.clione.entity.CloneStatus
+import io.github.t45k.clione.entity.InstancedCloneSets
 
 /**
  * Judgement condition:
+ * newCloneSets
  * all or all except one clone instances are ADD -> newly added
  * any clone instance is MODIFY -> inconsistently modified
  * else -> new clone added
+ *
+ * oldCloneSets
+ * some clone instances are DEL and others are STABLE -> unmerged
  */
 class SummaryGenerator {
-    fun generate(cloneSets: List<List<CloneInstance>>): Summary {
-        if (cloneSets.isEmpty()) {
-            return Summary(emptyList(), emptyList(), emptyList())
+    fun generate(oldCloneSets: InstancedCloneSets, newCloneSets: InstancedCloneSets): Summary {
+        val unmergedCloneSets: InstancedCloneSets = oldCloneSets.filter { cloneSet ->
+            val stableClones = cloneSet.count { it.status == CloneStatus.STABLE }
+            stableClones > 0 && stableClones + cloneSet.count { it.status == CloneStatus.DELETE } == cloneSet.size
+        }
+        if (unmergedCloneSets.isEmpty() && newCloneSets.isEmpty()) {
+            return Summary(emptyList(), emptyList(), emptyList(), emptyList())
         }
 
         val inconsistentlyChangedCloneSets = mutableListOf<List<CloneInstance>>()
         val newlyAddedCloneSets = mutableListOf<List<CloneInstance>>()
         val newCloneAddedCloneSets = mutableListOf<List<CloneInstance>>()
-        cloneSets.forEach { cloneSet ->
+        newCloneSets.forEach { cloneSet ->
             when {
                 cloneSet.count { it.status == CloneStatus.ADD } >= cloneSet.size - 1 -> newlyAddedCloneSets.add(cloneSet)
                 cloneSet.any { it.status == CloneStatus.MODIFY } -> inconsistentlyChangedCloneSets.add(cloneSet)
@@ -26,7 +35,7 @@ class SummaryGenerator {
             }
         }
 
-        return Summary(inconsistentlyChangedCloneSets, newlyAddedCloneSets, newCloneAddedCloneSets)
+        return Summary(inconsistentlyChangedCloneSets, newlyAddedCloneSets, newCloneAddedCloneSets, unmergedCloneSets)
     }
 }
 
@@ -34,14 +43,17 @@ class SummaryGenerator {
  * inconsistentlyChangedCloneSets: clone sets in which one or more clone instances are changed, but the others are stable.
  * newlyCreatedCloneSets: clone sets which newly appeared between the pull request.
  * newCloneAddedCloneSets: clone sets created by adding different clone instances to the original clone sets.
+ * unmergedCloneSets: clone sets in which some clone instances are merged but others are stable.
  */
 data class Summary(
-    val inconsistentlyChangedCloneSets: List<List<CloneInstance>>,
-    val newlyCreatedCloneSets: List<List<CloneInstance>>,
-    val newCloneAddedCloneSets: List<List<CloneInstance>>
+    val inconsistentlyChangedCloneSets: InstancedCloneSets,
+    val newlyCreatedCloneSets: InstancedCloneSets,
+    val newCloneAddedCloneSets: InstancedCloneSets,
+    val unmergedCloneSets: InstancedCloneSets
 ) {
     fun isAllEmpty(): Boolean =
         inconsistentlyChangedCloneSets.isEmpty() &&
             newlyCreatedCloneSets.isEmpty() &&
-            newCloneAddedCloneSets.isEmpty()
+            newCloneAddedCloneSets.isEmpty() &&
+            unmergedCloneSets.isEmpty()
 }

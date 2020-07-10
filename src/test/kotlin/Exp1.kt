de9b77872e1d246e0733583a4cc1060f8e896104
T45K <tasktas9@gmail.com>
2020-07-10 11:17:19 +0900

    for exp1

---
+++ src/test/kotlin/Exp1.kt
@@ -0,0 +1,52 @@
+import io.github.t45k.clione.controller.GitController
+import io.github.t45k.clione.controller.PullRequestController
+import io.github.t45k.clione.core.CloneTracker
+import io.github.t45k.clione.core.RunningConfig
+import org.kohsuke.github.GHIssueState
+import org.kohsuke.github.GitHubBuilder
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.test.Test
+
+class Exp1 {
+
+    @Test
+    fun investigateMaintenanceTargetClonesProportion() {
+        val repositoryFullName = "dnsjava/dnsjava"
+        var javaFileChangedPRCount = 0
+        var targetClonesPRCount = 0
+        val prInfos = mutableListOf<String>()
+        GitHubBuilder.fromEnvironment()
+            //.withPassword("T45K","")
+            .build()
+            .getRepository(repositoryFullName)
+            .getPullRequests(GHIssueState.CLOSED)
+            .filter { it.isMerged }
+            .forEach { ghPR ->
+                val pullRequest = PullRequestController(ghPR)
+                GitController.cloneIfNotExists(repositoryFullName, "", pullRequest).use { git ->
+                    val (tmp, head) = pullRequest.getComparisonCommits()
+                    val base = git.getCommonAncestorCommit(tmp, head)
+                    val (oldChangedFiles, newChangedFiles) = git.findChangedFiles(base, head)
+                    val isJavaFileChanged = setOf(*oldChangedFiles.toTypedArray(), *newChangedFiles.toTypedArray())
+                        .any { it.toString().contains("src/main/java") && it.toString().endsWith(".java") }
+                    if (!isJavaFileChanged) {
+                        return@forEach
+                    }
+
+                    javaFileChangedPRCount++
+                    val config = RunningConfig("src/main/java")
+                    val tracker = CloneTracker(git, pullRequest, config)
+                    val (oldClones, newClones) = tracker.track()
+                    if (oldClones.isEmpty() && newClones.isEmpty()) {
+                        return@forEach
+                    }
+
+                    targetClonesPRCount++
+                    prInfos.add("${pullRequest.number} $base $head")
+                }
+            }
+
+        Files.writeString(Path.of("result"), prInfos.joinToString("\n"))
+    }
+}

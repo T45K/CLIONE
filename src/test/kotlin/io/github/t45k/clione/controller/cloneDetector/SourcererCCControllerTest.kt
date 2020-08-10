a5e0db0fe441de90be8ace8eb13a2f16696eb94d
T45K <tasktas9@gmail.com>
2020-08-10 17:48:31 +0900

    add test

--- src/test/kotlin/io/github/t45k/clione/controller/cloneDetector/SourcererCCControllerTest.kt
+++ src/test/kotlin/io/github/t45k/clione/controller/cloneDetector/SourcererCCControllerTest.kt
@@ -5,12 +5,13 @@ import io.github.t45k.clione.controller.PullRequestController
 import io.github.t45k.clione.core.Language
 import io.github.t45k.clione.core.RunningConfig
 import io.github.t45k.clione.entity.CloneSets
-import io.github.t45k.clione.entity.CloneStatus
+import io.github.t45k.clione.entity.CloneStatus.DELETE
 import io.github.t45k.clione.util.generatePRMock
 import io.github.t45k.clione.util.toRealPath
 import java.nio.file.Path
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 internal class SourcererCCControllerTest {
 
@@ -27,10 +28,25 @@ internal class SourcererCCControllerTest {
         val cloneDetector = SourcererCCController(git.getProjectPath().resolve(config.src), config)
 
         val changedFiles: Set<Path> = setOf("./storage/T45K/trial_0/src/Sample.java".toRealPath())
-        val (cloneSets: CloneSets, _) = cloneDetector.execute(changedFiles, CloneStatus.DELETE)
+        val (cloneSets: CloneSets, _) = cloneDetector.execute(changedFiles, DELETE)
+        git.close()
 
         assertEquals(listOf(setOf(1, 2)), cloneSets)
+    }
 
+    @Test
+    fun testOverlapping() {
+        val pr = generatePRMock(REPOSITORY_FULL_NAME, 0, "d6f4884a0dfcb37c57b805e1f3d22115d0a76fa2")
+        val git = GitController.cloneIfNotExists(REPOSITORY_FULL_NAME, "", pr)
+        val config = RunningConfig("src")
+        val cloneDetector = SourcererCCController(git.getProjectPath().resolve(config.src), config)
+
+        val changedFiles: Set<Path> = setOf("./storage/T45K/trial_0/src/Sample.java".toRealPath())
+        val (cloneSets: CloneSets, _) = cloneDetector.execute(changedFiles, DELETE)
         git.close()
+
+        // Block 1 contains block 2.
+        // So, these blocks should not be detected as clones.
+        assertNotEquals(listOf(setOf(1, 2)), cloneSets)
     }
 }
